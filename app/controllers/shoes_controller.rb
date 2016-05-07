b13ed040947fdfe9a1f89f438cf93b09e43237fb
Smith <smith@futureworkz.com>
2016-05-07 17:11:24 +0700

    Set pagination for views shoes

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -2,7 +2,7 @@ class ShoesController < ApplicationController
   def index
     @shoe_filter        = ShoeFilter.new(filter_params, cookies[:sortby])
 
-    @shoes              = @shoe_filter.search
+    @shoes              = @shoe_filter.search.paginate(page: params[:page], per_page: 6)
 
     @brands             = Brand.all.map{ |c| [c.id, c.title] }
   end
