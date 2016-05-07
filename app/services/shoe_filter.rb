f3b7b7c366800763a27c420d34ac129e580275d1
Smith <smith@futureworkz.com>
2016-05-07 14:29:22 +0700

    Update services#shoe_filter with min_price and max_price

--- app/services/shoe_filter.rb
+++ app/services/shoe_filter.rb
@@ -1,18 +1,24 @@
 class ShoeFilter
   include ActiveModel::Conversion
   extend  ActiveModel::Naming
-  attr_accessor :brand_ids
+  attr_accessor :brand_ids, :min_price, :max_price
 
   def initialize(option)
     return if option.nil?
 
     @brand_ids        = option[:brand_ids]
+    @min_price        = option[:min_price]
+    @max_price        = option[:max_price]
   end
 
   def search
     shoes = Shoe.all
 
     shoes = shoes.where(brand: brand_ids) if brand_ids.present? and brand_ids.any? &:present?
+
+    shoes = shoes.where('price >= ?', min_price) if min_price.present?
+    
+    shoes = shoes.where('price <= ?', max_price) if max_price.present?
     
     shoes
   end
